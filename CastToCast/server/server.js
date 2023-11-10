538b09808823e5f9e5495a98ceb59bdf693efb5a
guytamari <guy8267@gmail.com>
2023-11-10 13:19:33 +0200

    101319

--- CastToCast/server/server.js
+++ CastToCast/server/server.js
@@ -36,18 +36,45 @@ app.get("/api/getMoviesByActor/:actorid", async (req, res) => {
   }
 });
 
-//get actor by actor id 
+<<<<<<< HEAD
+//get actor by actor id
 app.get("/api/getActor/:actorid", async (req, res) => {
   try {
     const apiKey = "a3d7cc20442b9124e3ef7d9d2f45a2f9";
     const actorId = req.params.actorid;
-    const response = await axios.get(`https://api.themoviedb.org/3/person/${actorId}api_key=${apiKey}`);
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/person/${actorId}?api_key=${apiKey}`
+    );
+=======
+//get actor by actor id 
+app.get("/api/getActor/:actorId", async (req, res) => {
+  try {
+    const apiKey = "a3d7cc20442b9124e3ef7d9d2f45a2f9";
+    const actorId = req.params.actorId;
+    const response = await axios.get(`https://api.themoviedb.org/3/person/${actorId}?api_key=${apiKey}`);
+>>>>>>> 274115817ddae466fc1985fe19442901d144149b
     const actor = response.data;
     res.json(actor);
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+
+app.get("/api/getMovie/:movieId", async (req, res) => {
+  try {
+    const apiKey = "a3d7cc20442b9124e3ef7d9d2f45a2f9";
+    const movieId = req.params.movieId;
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`);
+    const movie = response.data;
+    res.json(movie);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+
 
 //get top actors
 app.get("/api/getTopActors/", async (req, res) => {
